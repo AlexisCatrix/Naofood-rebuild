53811c56c13d442b343784c77577a647fc001e13
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-08-03 16:18:22 +0200

    Html for footer wrote

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,1 +1,48 @@
-footer
+<footer class="footer">
+    <div class="footer__large">
+        <div class="footer__large-content optimal-width">
+            <a href="/">
+                <img class="h-[100px]" src="/image/NaoBlanc.png" alt="Naofood - Logo"/>
+            </a>
+            <div class="footer__large-content-links">
+                <div class="footer__large-content-single">
+                    <h6>Réseau sociaux</h6>
+                        <div class="socials flex">
+                            <a target="_blank" rel="noopener" href="https://www.facebook.com/nao.food/">
+                              <i class="fab fa-facebook-f"></i>
+                            </a> 
+                            <a target="_blank" rel="noopener" href="https://www.instagram.com/nao.food/">
+                              <i class="fab fa-instagram"></i>
+                            </a> 
+                            <a target="_blank" rel="noopener" href="https://twitter.com/nao_food">
+                              <i class="fab fa-twitter"></i>
+                            </a>
+                        </div>     
+                </div>
+                <div class="footer__large-content-single">
+                    <h6>À propos</h6>
+                    <a href="https://naofood.fr/qui-sommes-nous">
+                        Qui sommes-nous ?
+                    </a>
+                    <a href="https://naofood.fr/contact">
+                        Contact
+                    </a>
+                </div>
+                <div class="footer__large-content-single">
+                    <h6>Informations</h6>
+                    <a href="https://naofood.fr/mentions-legales">
+                        Mentions légales
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer__thin">
+        <div class="footer__thin-content">
+            ©2021 - Naofood. Tous droits réservés. Site web réalisé par
+            <a href="https://start-win.fr" target="_blank">
+            Start-Win
+            </a> 
+        </div>
+    </div>
+</footer>
