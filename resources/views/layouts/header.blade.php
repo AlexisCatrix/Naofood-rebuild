60554e795458785e785b0b6f1b8bd8e1f81d57d4
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-08-03 16:36:51 +0200

    Header tag added

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,31 +1,33 @@
 
 <div>
-    <div class="header__content">
-        <a href="/">
-            <img src="/image/logo-naofood.svg" alt="Naofood - Logo" class="h-[60px] max-height-full "/>
-        </a>
-        <nav class="header__content-menu">
-            <ul class="flex justify-start">
-                <li class="header__content-menu-single">
-                    <a href="/accueil">Accueil</a>
-                </li> 
-                <li class="header__content-menu-single">
-                    <a href="/nos-restaurants">Nos restaurants</a>
-                </li>
-                <li class="header__content-menu-single">
-                    <a href="/nos-associations">Nos associations</a>
-                </li>
-                <li class="header__content-menu-single">
-                    <a href="/nos-partenaires">Nos partenaires</a>
-                </li>
-                <li class="header__content-menu-single">
-                    <a href="/qui-sommes-nous">Les Coursiers Nao</a>
-                </li>
-                <li class="header__content-menu-single">
-                    <a href="/contact">Contact</a>
-                </li>
-            </ul>
-            <a class="call-to-action">Commander</a>
-        </nav>
-    </div>
+    <header class="header">
+        <div class="header__content">
+            <a href="/">
+                <img src="/image/logo-naofood.svg" alt="Naofood - Logo" class="h-[60px] max-height-full "/>
+            </a>
+            <nav class="header__content-menu">
+                <ul class="flex justify-start">
+                    <li class="header__content-menu-single">
+                        <a href="/accueil">Accueil</a>
+                    </li> 
+                    <li class="header__content-menu-single">
+                        <a href="/nos-restaurants">Nos restaurants</a>
+                    </li>
+                    <li class="header__content-menu-single">
+                        <a href="/nos-associations">Nos associations</a>
+                    </li>
+                    <li class="header__content-menu-single">
+                        <a href="/nos-partenaires">Nos partenaires</a>
+                    </li>
+                    <li class="header__content-menu-single">
+                        <a href="/qui-sommes-nous">Les Coursiers Nao</a>
+                    </li>
+                    <li class="header__content-menu-single">
+                        <a href="/contact">Contact</a>
+                    </li>
+                </ul>
+                <a class="call-to-action">Commander</a>
+            </nav>
+        </div>
+    </header>    
 </div>
